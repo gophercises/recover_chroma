1e9813c4536224fc8bdf6c729b7567fd31b59950
Jon Calhoun <joncalhoun@gmail.com>
2018-04-30 13:11:12 -0400

    Finished p3

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"runtime/debug"
+	"strings"
 
 	"github.com/alecthomas/chroma/quick"
 )
@@ -45,7 +47,7 @@ func devMw(app http.Handler) http.HandlerFunc {
 				stack := debug.Stack()
 				log.Println(string(stack))
 				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprintf(w, "<h1>panic: %v</h1><pre>%s</pre>", err, string(stack))
+				fmt.Fprintf(w, "<h1>panic: %v</h1><pre>%s</pre>", err, makeLinks(string(stack)))
 			}
 		}()
 		app.ServeHTTP(w, r)
@@ -67,4 +69,24 @@ func funcThatPanics() {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1>Hello!</h1>")
+}
+
+func makeLinks(stack string) string {
+	lines := strings.Split(stack, "\n")
+	for li, line := range lines {
+		if len(line) == 0 || line[0] != '\t' {
+			continue
+		}
+		file := ""
+		for i, ch := range line {
+			if ch == ':' {
+				file = line[1:i]
+				break
+			}
+		}
+		v := url.Values{}
+		v.Set("path", file)
+		lines[li] = "\t<a href=\"/debug/?" + v.Encode() + "\">" + file + "</a>" + line[len(file)+1:]
+	}
+	return strings.Join(lines, "\n")
 }
